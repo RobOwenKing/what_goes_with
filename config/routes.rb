a043f5b784e591f1e2b0396e1965c65f519e39f8
Robert King <robert.o.king@hotmail.co.uk>
2020-05-28 20:08:03 +0200

    Generated Food model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/show'
+  get 'foods/new'
+  get 'foods/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
