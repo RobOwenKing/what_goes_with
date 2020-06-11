9a2276e8d154edaa129b0617c9caa4828f1ef998
Robert King <robert.o.king@hotmail.co.uk>
2020-06-12 00:54:28 +0200

    Migration to create Reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_181120) do
+ActiveRecord::Schema.define(version: 2020_06_09_114613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 2020_06_03_181120) do
     t.index ["ingredient_2_id"], name: "index_pairs_on_ingredient_2_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "pair_id"
+    t.string "rating"
+    t.string "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pair_id"], name: "index_reviews_on_pair_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +57,6 @@ ActiveRecord::Schema.define(version: 2020_06_03_181120) do
 
   add_foreign_key "pairs", "ingredients", column: "ingredient_1_id"
   add_foreign_key "pairs", "ingredients", column: "ingredient_2_id"
+  add_foreign_key "reviews", "pairs"
+  add_foreign_key "reviews", "users"
 end
