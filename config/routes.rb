b3ae4c19959de573c4004b480cce34a9238e07e3
Robert King <robert.o.king@hotmail.co.uk>
2020-06-17 00:29:35 +0200

    Started Pairs show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'pairs/show'
-  get 'pairs/create'
   devise_for :users
   root to: 'pages#home'
 
   resources :ingredients, only: [ :index, :new, :create ]
   get 'ingredients/:name', to: 'ingredients#show'
+
+  resources :pairs, only: [ :show, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
