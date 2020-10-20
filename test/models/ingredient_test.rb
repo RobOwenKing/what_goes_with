5033f79423c7819eef8c003df59b9e3bfef3eaf7
Robert King <robert.o.king@hotmail.co.uk>
2020-10-20 16:46:47 +0200

    Added: Ingredient validations tests

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  test "ingredients must have a name" do
+  test "Ingredients must have a name" do
     ingredient = Ingredient.new(colour: '#FFFFFF')
+    assert_not ingredient.save
+  end
+
+  test "Ingredient names must be unique" do
+    Ingredient.create(name: 'Aubergine', colour: '#FFFFFF')
+    ingredient = Ingredient.new(name: 'Aubergine', colour: '#FFFFFF')
+    assert_not ingredient.save
+  end
+
+  test "Ingredients must have a colour" do
+    ingredient = Ingredient.new(name: 'Mint')
     assert_not ingredient.save
   end
 end
