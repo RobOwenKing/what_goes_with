bfad2b875276b519b79d758a47b636674e2e208f
Robert King <robert.o.king@hotmail.co.uk>
2020-10-20 18:12:03 +0200

    Refactored: Ingredient unit testsfor more specific asserts

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -3,17 +3,20 @@ require 'test_helper'
 class IngredientTest < ActiveSupport::TestCase
   test "Ingredients must have a name" do
     ingredient = Ingredient.new(colour: '#FFFFFF')
-    assert_not ingredient.save
+    ingredient.valid?
+    assert_not_empty ingredient.errors[:name]
   end
 
   test "Ingredient names must be unique" do
     Ingredient.create(name: 'Aubergine', colour: '#FFFFFF')
     ingredient = Ingredient.new(name: 'Aubergine', colour: '#FFFFFF')
-    assert_not ingredient.save
+    ingredient.valid?
+    assert_not_empty ingredient.errors[:name]
   end
 
   test "Ingredients must have a colour" do
     ingredient = Ingredient.new(name: 'Mint')
-    assert_not ingredient.save
+    ingredient.valid?
+    assert_not_empty ingredient.errors[:colour]
   end
 end
