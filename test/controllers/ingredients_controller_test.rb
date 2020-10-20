9f64bda5e39fcf00e353b01b3724146e26a3e944
Robert King <robert.o.king@hotmail.co.uk>
2020-10-20 18:02:45 +0200

    Added: First Ingredient controller test

--- test/controllers/ingredients_controller_test.rb
+++ test/controllers/ingredients_controller_test.rb
@@ -2,23 +2,23 @@ require 'test_helper'
 
 class IngredientsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get ingredients_index_url
+    get ingredients_url
     assert_response :success
   end
 
-  test "should get show" do
-    get ingredients_show_url
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get ingredients_show_url
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get ingredients_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get ingredients_new_url
+  #   assert_response :success
+  # end
 
-  test "should get create" do
-    get ingredients_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get ingredients_create_url
+  #   assert_response :success
+  # end
 
 end
