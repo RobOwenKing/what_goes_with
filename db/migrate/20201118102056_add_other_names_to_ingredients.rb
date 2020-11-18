c2d08bafa85163d708e9541665a0004fa64bb427
Robert King <robert.o.king@hotmail.co.uk>
2020-11-18 14:10:34 +0100

    Added: Indexes to AKA/EG migration

--- db/migrate/20201118102056_add_other_names_to_ingredients.rb
+++ db/migrate/20201118102056_add_other_names_to_ingredients.rb
@@ -1,6 +1,9 @@
 class AddOtherNamesToIngredients < ActiveRecord::Migration[5.2]
   def change
-    add_column :ingredients, :akas, :string
-    add_column :ingredients, :egs, :string
+    add_column :ingredients, :aka, :string
+    add_column :ingredients, :eg, :string
+
+    add_index :ingredients, :aka
+    add_index :ingredients, :eg
   end
 end
