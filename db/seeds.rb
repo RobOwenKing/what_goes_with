dcba0192013eca9db3799655fdeffe25f1961014
Robert King <robert.o.king@hotmail.co.uk>
2020-11-23 21:43:35 +0100

    Added: Seeds with Eg and Aka

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 Ingredient.create(name: 'Lemon', colour: '#FFD538', image: 'y_spQMQTFjs')
 Ingredient.create(name: 'Spinach', colour: '#5AB824', image: '6AAptb2kBak')
 Ingredient.create(name: 'Tomato', colour: '#E33F20', image: 'dJHdolN553o')
+Ingredient.create(name: 'Blue Cheese', eg: 'Stilton, Roquefort, Gorgonzola', colour: '#E33F20')
+Ingredient.create(name: 'Courgette', aka: 'Zucchini', colour: '#E33F20')
